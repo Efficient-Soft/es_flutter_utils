6636e7b16023a389d0e7806285d8c846c10bbe78
ThetPaingWin <thetpaingwin.dev@gmail.com>
2026-06-29 13:49:39 +0630

    Update es_flutter_utils.dart

--- lib/es_flutter_utils.dart
+++ lib/es_flutter_utils.dart
@@ -4,3 +4,5 @@ export 'src/models/mystate.dart';
 export 'src/models/widget_metadata.dart';
 export 'src/models/notification_model.dart';
 export 'src/database/notification_db_manager.dart';
+export 'src/notification/fcm_notification_v2_manager.dart';
+export 'src/notification/local_notification_manager.dart';
