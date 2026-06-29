eb3ed9de7ac511118ee7dc9a52fa8642dfd4288f
ThetPaingWin <thetpaingwin.dev@gmail.com>
2026-06-29 16:24:51 +0630

    Update local_fcm_token_retriever.dart

--- lib/src/notification/local_fcm_token_retriever.dart
+++ lib/src/notification/local_fcm_token_retriever.dart
@@ -1,24 +1,24 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalFcmTokenRetriever {
-  static const _itemKey = 'qf_fcm_key';
+  static String itemKey = 'fcm_token_key';
 
   Future<String?> get() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_itemKey);
+    return prefs.getString(itemKey);
   }
 
   Future<void> save(String? data) async {
     final prefs = await SharedPreferences.getInstance();
     if (data == null) {
-      await prefs.remove(_itemKey);
+      await prefs.remove(itemKey);
     } else {
-      await prefs.setString(_itemKey, data);
+      await prefs.setString(itemKey, data);
     }
   }
 
   Future<void> delete() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_itemKey);
+    await prefs.remove(itemKey);
   }
 }
