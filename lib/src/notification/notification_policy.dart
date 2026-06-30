b78419cb10e05960ffd65b8529892da7ad476db3
ThetPaingWin <thetpaingwin.dev@gmail.com>
2026-06-30 10:24:55 +0630

    Delete notification_policy.dart

--- lib/src/notification/notification_policy.dart
+++
@@ -1,9 +0,0 @@
-import 'package:es_flutter_utils/es_flutter_utils.dart';
-
-abstract class NotificationPolicy {
-  bool shouldSave(NotificationModel notification);
-  bool shouldShowLocalNotification(NotificationModel notification);
-  bool shouldPlayAlert(NotificationModel notification);
-  String channelIdFor(NotificationModel notification);
-  String? soundFor(NotificationModel notification);
-}
